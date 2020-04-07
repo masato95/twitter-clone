5b437e1e229d9dbf6dad69aca4ec6c984f743276
hiroyuki.masato <masato0950@gmail.com>
2020-04-07 09:30:33 +0000

    exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :forwards, through: :favorites, source: :micropost
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :backwords, through: :reverses_of_favorite, source: :user
   
   def follow(other_user)
     unless self == other_user
@@ -29,5 +33,20 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(micropost)
+    #unless self.forwards == micropost
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    #end
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def foward?(micropost)
+    self.forwards.include?(micropost)
   end
 end
